e43004cf7210da5c4944f53d590e8b7c7117dec1
Greatisland <100889780+Greatisland@users.noreply.github.com>
2022-03-15 12:31:30 +0900

    Add files via upload

--- js/all_layout02.js
+++ js/all_layout02.js
@@ -1,5 +1,22 @@
 $(function(){
     /////////////////////////
-$('.headerWrap').css('color','#369');
+
+//var left = 1;
+
+//function mainSlide(){
+//    $('.mainSlide').css({left:-(left*100)+"%"});
+//    left=left+1;
+//    if(left===3) {left=0}
+//}
+
+//setInterval(mainSlide,2000)
+$('.mainSlide').slick({
+    arrow:false,
+    autoplay:true,
+    autoplaySpeed:4000,
+    speed:2000,
+});
+
+
     /////////////////////////
 });
