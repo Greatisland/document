1b233060ba7bf822f428c617ef17ebf5e2db06f5
Greatisland <100889780+Greatisland@users.noreply.github.com>
2022-05-18 17:07:24 +0900

    Add files via upload

--- login/js/test.js
+++ login/js/test.js
@@ -1,37 +1,61 @@
 $(function(){
 
-    let login = $('.login_btn')
+    let loginBtn = $('.login_btn')
     let popUp = $('.popup_login')
     let wingBtn = $('.wing_btn')
     let wingBanner = $('.wing_banner')
 
     const id = 'green'
-    const pw = 12345
+    const pw = 12345 
 
-    let state = false
-    
-    
-    login.on('click',function(){
-        if(!state){
-        popUp.fadeIn(500).css('display','flex').draggable()
+    let state = false //로그인 창 상태변수
+    let login = false // 로그인 상태변수
+    let i = 5 // 로그인 시도 가능 카운트
+
+    $('.login').click(function(loginTry){
+
+        let userId = $('.id').val()
+        let userPw = $('.pw').val()
+        
+        if(id==userId  && pw==userPw && i>0){
+            loginBtn.text('로그아웃')
+            alert('로그인 성공')
+            login = true
+            $('.id').val('')
+            $('.pw').val('')
+            $('.close').click()
+        }else if(id!=userId && pw==userPw && i>0){
+            alert('아이디가 틀렸습니다.'+i+'회 남았습니다.')
+            return i-=1
+        }else if(id==userId && pw!=userPw && i>0){
+            alert('비밀번호가 틀렸습니다.'+i+'회 남았습니다.')
+            return i-=1
+        }else if (id!=userId && pw!=userPw && i>0){
+            alert('아이디와 비밀번호가 틀렸습니다.'+i+'회 남았습니다.')
+            return i-=1
+        }else {
+            alert('더 이상 로그인 할 수 없습니다.')
         }
-        state = true
-        $('body>*').not('.popup_login').css('opacity','0.1')
-        $('.login').click(function(){
+    })//로그인 이벤트 함수
 
-            let userId = $('.id').val()
-            let userPw = $('.pw').val()
 
-            if(id==userId  || pw==userPw){
-                alert('로그인 성공')
-            }else{
-                alert('로그인에 실패하였습니다.')
-            }
-        })
+    loginBtn.on('click',function(){
+        if(!state && !login){
+            popUp.fadeIn(500).css('display','flex').draggable()
+            state = true
+            $('body>*').not('.popup_login').css('opacity','0.1')
+            loginTry()
+        }else if(!state && login){
+            login = false
+            loginBtn.text('로그인')
+            alert('로그아웃 하였습니다.')
+        }
     })
 
     $('.close').on('click',function(){
         popUp.fadeOut(500)
+        $('.id').val('')
+        $('.pw').val('')
         $('body>*').not('.popup_login').css('opacity','1')
         state = false
     })
